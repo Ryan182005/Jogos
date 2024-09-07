64fa6d4837d1d7d402692a79557205bb93573d2b
Ryan <ryanr252@hotmail.com>
2024-09-07 15:54:20 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo : "Elden Ring",
+        descricao : "Elden Ring: Um Mundo Aberto de Fantasia Sombria Elden Ring é um RPG de ação desenvolvido pela FromSoftware, conhecida por criar experiências de jogo desafiadoras e imersivas. Ambientado em um mundo aberto vasto e interconectado, o jogo convida os jogadores a  explorar as Terras Intermédias, um reino assolado pela guerra e pela ruína.",
+        link : "https://store.steampowered.com/agecheck/app/1245620/"
+        tags : "rpg from jogo"
+    },   
+    {
+        titulo : "God of War",
+        descricao : "God of War (2018) marca um novo capítulo na saga de Kratos, o lendário Deus da Guerra. Abandonando a mitologia grega, o jogo nos transporta para os reinos nórdicos, onde um Kratos mais velho e cansado busca redenção ao lado de seu filho, Atreus.",
+        link : "https://store.steampowered.com/agecheck/app/1593500/"
+    },
+    {
+        titulo : "Black Myth Wukong",
+        descricao : "Black Myth: Wukong é um ambicioso RPG de ação que reimagina a clássica história chinesa da Jornada para o Oeste, com um toque moderno e gráficos de tirar o fôlego. O jogo coloca os jogadores no papel de Sun Wukong, o Rei Macaco, uma figura lendária da mitologia chinesa, conhecida por sua força sobrenatural e travessura.",
+        link : "https://store.steampowered.com/agecheck/app/2358720/"
+    },
+    {
+        titulo : "Red Dead Redemption 2",
+        descricao : "Red Dead Redemption 2 é uma épica aventura em mundo aberto ambientada no selvagem oeste americano do final do século XIX. O jogo te coloca no papel de Arthur Morgan, um fora da lei membro da gangue de Van der Linde.",
+        link : "https://store.steampowered.com/agecheck/app/1174180/"
+    },
+    {
+        titulo : "Dying Light",
+        descricao : "Dying Light é um jogo de ação e sobrevivência em primeira pessoa ambientado em um mundo aberto infestado por zumbis. Você assume o papel de um agente infiltrado em uma cidade devastada por um vírus mortal, onde a noite é governada por hordas de infectados extremamente perigosos.",
+        link : "https://store.steampowered.com/agecheck/app/239140/"
+    },
+    {
+        titulo : "Moonlighter",
+        descricao : "Moonlighter é uma experiência de jogo única que combina a emoção de hack-and-slash com a satisfação de gerenciar uma loja.",
+        link : "https://store.steampowered.com/app/606150/Moonlighter/"
+    },
+    {
+        titulo : "GTA V",
+        descricao : "Grand Theft Auto V é um jogo de mundo aberto que coloca você no controle de três protagonistas: um malandro de rua, um ladrão de bancos aposentado e um psicopata aterrorizante. Ambientado na vibrante e caótica cidade de Los Santos, inspirada em Los Angeles, o jogo te convida a explorar uma metrópole repleta de oportunidades e perigos.",
+        link : "https://store.steampowered.com/agecheck/app/271590/",
+    }
+];
+
